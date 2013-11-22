dba46a5fd2dc38ff67aed49f60efa86174d5fc68
Brian T. Webb <btwebb@gmail.com>
2013-11-22 16:17:12 -0500

    Updated chefspec per Seth's guidance. Cookbook now fully tested.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -9,4 +9,8 @@ describe 'appserv-tomcat::default' do
   it 'installs tomcat6' do
     expect(chef_run).to install_package('tomcat6')
   end
+
+  it 'starts tomcat6' do
+    expect(chef_run).to start_service('tomcat6')
+  end
 end
