85e38835d850ff61cf25b52d3387547257eab926
C62963 <bwebb@bbandt.com>
2014-09-26 12:02:48 -0400

    Completed stand alone install of Artifactory Pro. Also, a bit of Artifactory HA done.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,17 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# If it's an HA node, have to mount NFS
+if (node[:artifactory][:is_ha_node]) 
+  directory node[:artifactory][:ha_mount_point] do
+    action :create
+    not_if do ::File.directory?(node[:artifactory][:ha_mount_point]) end
+  end
+  
+  execute "mount #{node[:artifactory][:ha_nfs_server]} #{[:artifactory][:ha_mount_point]}" do
+  end
+end
+
 # Pull down the Artifactory RPM
 remote_file node[:artifactory][:rpm_local_path] do
   source node[:artifactory][:rpm_url]
@@ -36,4 +47,19 @@ end
 # Start up the service
 service node[:artifactory][:service_name] do
   action :start
+end
+
+# Sleep a bit so the Artifactory service can come up
+ruby_block "Waiting for Artifactory service" do
+  block do
+    artup = false
+    while not artup do
+      begin
+        RestClient.get("http://admin:password@localhost:8081/artifactory/api/system")
+        artup = true
+      rescue
+        sleep(2)
+      end
+    end
+  end
 end
